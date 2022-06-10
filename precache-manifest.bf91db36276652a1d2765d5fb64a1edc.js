cecf66dd791890ee52d7afa1d21fc93d695edb33
Kelvin Mitchell <kelvin.mitchell@doane.edu>
2022-06-10 12:20:01 -0500

    Updates

---
+++ precache-manifest.bf91db36276652a1d2765d5fb64a1edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d50fe985d7741e4a5f4",
+    "url": "/exampleApp/static/css/main.7fdb1e70.chunk.css"
+  },
+  {
+    "revision": "7d50fe985d7741e4a5f4",
+    "url": "/exampleApp/static/js/main.7d50fe98.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/exampleApp/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/exampleApp/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "306a1632e1ad5826679e",
+    "url": "/exampleApp/static/js/runtime~main.306a1632.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/exampleApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "11786aa9bf03ad8fdf6011c0d78e6a3c",
+    "url": "/exampleApp/index.html"
+  }
+];
