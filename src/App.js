5d85696bde442ff0e93e9fca5194f3fa0e86ae24
Kelvin Mitchell <kelvin.mitchell@doane.edu>
2022-06-13 06:04:13 -0500

    monday morning push

--- src/App.js
+++ src/App.js
@@ -1,63 +1,123 @@
-import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import './index.css';
 
 
 const App = () => {
 	// HINT: each "item" in our list names a name,
 	// a boolean to tell if its been completed, and a quantity
-	const [items, setItems] = useState([
-	]);
-	const [items1, setItems1] = useState([
-	]);
+    const [items, setItems] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 	const [lists, setLists] = useState([]);
+    const [listValue, setListValue] = useState('');
+    const [boards, setBoards] = useState([]);
+    const [boardValue, setBoardValue] = useState('');
+
+    const reactBoardsArray = boards.map((data) => {
+        return (
+            <div class="card">
+                 <div class="col-sm-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <a class="btn btn-primary" onClick={() => handleAddList()} >Add List</a>
+                            <input value={listValue} id={listValue} onChange={(event) => setListValue(event.target.value)} className='add-item-input flex' placeholder='Name List' style={{ width: '3cm' }} />
+                            <h5 class="card-title">{listValue}</h5>
+                            <p class="card-text">
+                                <div className='add-item-box flex'>
+                                    <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input flex' placeholder='Add a task...' style={{ width: '3cm' }} />
+                                    <a class="btn btn-primary" onClick={() => handleAddButtonClick()}>Add Task</a>
+                                </div>
+                                <div className='item-list'>
+                                    {items.map((item, index) => (
+
+
+                                        <div className='item-container'>
+                                            <div className='item-name' >
+
+
+
+                                                <strong><span>{item.itemName}</span></strong>
+
+
+                                                <button key={index} className="item" onClick={() => handleRight(index)} class=" btn btn-outline-secondary btn-sm"> → </button>
+
+                                                <block>
+                                                    <button key={index} className="item" onClick={() => handleDelete(index)} class=" btn btn-outline-secondary btn-sm"> Delete Task </button>
+                                                </block>
+
+                                            </div>
+
+
+
+
+
+                                        </div>
+                                    ))}
+                                </div>
+
+
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div>{reactListArray}</div>
+                </div>
+
+                );
+    });
 
 	const reactListArray = lists.map((data) => {
 		return (
 			<div className='main-container'>
-				<div className='add-item-box'>
-					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
-					<button onClick={() => handleAddButtonClick()}> </button>
-				</div>
-				{lists.map((item, index) => (
-					<div className='item-list'>
-						{items.map((item, index) => (
-							<div className='item-container'>
-								<div className='item-name' onClick={() => toggleComplete(index)}>
-									{item.isSelected ? (
-										<>
+                <div class="col-sm-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <a class="btn btn-primary" onClick={() => handleAddList()} >Add List</a>
+                            <input value={listValue} id={listValue} onChange={(event) => setListValue(event.target.value)} className='add-item-input flex' placeholder='Name List' style={{ width: '3cm' }} />
+                            <h5 class="card-title">{listValue}</h5>
+                            <p class="card-text">
+                                <div className='add-item-box flex'>
+                                    <input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input flex' placeholder='Add a task...' style={{ width: '3cm' }} />
+                                    <a class="btn btn-primary" onClick={() => handleAddButtonClick()}>Add Task</a>
+                                </div>
+                                <div className='item-list'>
+                                    {items.map((item, index) => (
 
-											<span className='completed'>{item.itemName}</span>
-										</>
-									) : (
-										<>
 
-											<span>{item.itemName}</span>
-										</>
-									)}
-								</div>
-								<div className='quantity'>
-									<button onClick={() => handleQuantityDecrease(index)}>
-									</button>
-									<span> {item.quantity} </span>
-									<button onClick={() => handleQuantityIncrease(index)}>
-									</button>
-								</div>
-							</div>
-						))}
-					</div>
-				))}
-				<div className='total'>Total: {totalItemCount}</div>
+                                        <div className='item-container'>
+                                            <div className='item-name' >
+
+
+
+                                                <strong><span>{item.itemName}</span></strong>
+
+
+                                                <button key={index} className="item" onClick={() => handleRight(index)} class=" btn btn-outline-secondary btn-sm"> → </button>
+
+                                                <block>
+                                                    <button key={index} className="item" onClick={() => handleDelete(index)} class=" btn btn-outline-secondary btn-sm"> Delete Task </button>
+                                                </block>
+
+                                            </div>
+
+
+
+
+
+                                        </div>
+                                    ))}
+                                </div>
+
+
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
 			</div>
 		);
 	});
 
-	const [inputValue, setInputValue] = useState('');
-	const [totalItemCount, setTotalItemCount] = useState(0);
-
-	const [inputValue1, setInputValue1] = useState('');
-	const [totalItemCount1, setTotalItemCount1] = useState(0);
-
-	const [listValue, setListValue] = useState('');
 
 	const handleAddButtonClick = () => {
 		const newItem = {
@@ -70,7 +130,7 @@ const App = () => {
 
 		setItems(newItems);
 		setInputValue('');
-		calculateTotal();
+		
 	};
 	const handleAddList = () => {
 		const newList = {
@@ -83,55 +143,166 @@ const App = () => {
 
 		setLists(newLists);
 		setListValue('');
-	};
-	const handleAddButtonClick1 = () => {
-		const newItem1 = {
-			itemName: inputValue1,
-			quantity: 1,
-			isSelected: false,
-		};
+    };
 
-		const newItems1 = [...items1, newItem1];
+    const handleAddBoard = () => {
+        const newBoard = {
+            itemName: boardValue,
+            quantity: 1,
+            isSelected: false,
+        };
 
-		setItems1(newItems1);
-		setInputValue('');
-		calculateTotal();
-	};
+        const newBoards = [...boards, newBoard];
+
+        setBoards(newBoards);
+        setBoardValue('');
+    };
+
+    const handleDelete = (itemIndex) => {
+
+        let updatedItems = [...items];
+        updatedItems.splice(itemIndex, 1);
+        setItems(updatedItems);
+
+    }
+
+    const handleDeleteSprint = (itemIndex) => {
+
+        let updatedItems = [...nextSprints];
+        updatedItems.splice(itemIndex, 1);
+        setNextSprint(updatedItems);
+
+    }
+
+    const handleDeleteDev = (itemIndex) => {
+
+        let updatedItems = [...nextDevs];
+        updatedItems.splice(itemIndex, 1);
+        setDev(updatedItems);
+
+    }
+
+    const handleDeleteTest = (itemIndex) => {
+
+        let updatedItems = [...nextTests];
+        updatedItems.splice(itemIndex, 1);
+        setTest(updatedItems);
+
+    }
+
+    const handleDeleteReview = (itemIndex) => {
+
+        let updatedItems = [...nextReviews];
+        updatedItems.splice(itemIndex, 1);
+        setReview(updatedItems);
+
+    }
+
+    const handleDeleteDone = (itemIndex) => {
+
+        let updatedItems = [...nextDone];
+        updatedItems.splice(itemIndex, 1);
+        setNextSprint(updatedItems);
+
+    }
+
+    const [nextSprints, setNextSprint] = useState([]);
+    const [sprintValue, setSprintValue] = useState('');
+    const handleSprintButtonClick = () => {
+        const newSprint = {
+            itemName: sprintValue,
+            quantity: 1,
+            isSelected: false,
+        };
+
+        const newItems = [...nextSprints, newSprint];
+
+        setNextSprint(newItems);
+        setSprintValue('');
+
+    };
+
+    const [nextDevs, setDev] = useState([]);
+    const [devValue, setDevValue] = useState('');
+    const handleDevButtonClick = () => {
+        const newDev = {
+            itemName: devValue,
+            quantity: 1,
+            isSelected: false,
+        };
+
+        const newItems = [...nextDevs, newDev];
+
+        setDev(newItems);
+        setDevValue('');
+
+    };
+
+    const [nextTests, setTest] = useState([]);
+    const [testValue, setTestValue] = useState('');
+    const handleTestButtonClick = () => {
+        const newTest = {
+            itemName: testValue,
+            quantity: 1,
+            isSelected: false,
+        };
+
+        const newItems = [...nextTests, newTest];
+
+        setTest(newItems);
+        setTestValue('');
+
+    };
 
 
-	const handleQuantityIncrease = (index) => {
-		const newItems = [...items];
+    const [nextReviews, setReview] = useState([]);
+    const [reviewValue, setReviewValue] = useState('');
+    const handleReviewButtonClick = () => {
+        const newReview = {
+            itemName: reviewValue,
+            quantity: 1,
+            isSelected: false,
+        };
 
-		newItems[index].quantity++;
+        const newItems = [...nextReviews, newReview];
 
-		setItems(newItems);
-		calculateTotal();
-	};
+        setReview(newItems);
+        setReviewValue('');
 
-	const handleQuantityDecrease = (index) => {
-		const newItems = [...items];
+    };
 
-		newItems[index].quantity--;
+    const [nextDone, setDone] = useState([]);
+    const [doneValue, setDoneValue] = useState('');
+    const handleDoneButtonClick = () => {
+        const newDone = {
+            itemName: doneValue,
 
-		setItems(newItems);
-		calculateTotal();
-	};
+        };
 
-	const toggleComplete = (index) => {
-		const newItems = [...items];
+        const newItems = [...nextDone, newDone];
 
-		newItems[index].isSelected = !newItems[index].isSelected;
+        setDone(newItems);
+        setDoneValue('');
 
-		setItems(newItems);
-	};
+    };
 
-	const calculateTotal = () => {
-		const totalItemCount = items.reduce((total, item) => {
-			return total + item.quantity;
-		}, 0);
+    const handleRight = (itemIndex) => {
 
-		setTotalItemCount(totalItemCount);
-	};
+        const newSprint = items[itemIndex];
+
+        const newItems = [...nextSprints, newSprint];
+
+        setNextSprint(newItems);
+
+        let updatedItems = [...items];
+        updatedItems.splice(itemIndex, 1);
+        setItems(updatedItems);
+
+
+
+
+    }
+
 
 	return (
 		<div className='app-background'>
@@ -139,14 +310,18 @@ const App = () => {
 
 			
 
+			<row>
 
+                <a class="btn btn-primary" onClick={() => handleAddBoard()} >Add Board</a>
+				
+					 
+              
+                
+			    
 
-			<div className='add-item-box'>
-				<input value={listValue} onChange={(event) => setListValue(event.target.value)} className='add-item-input' placeholder='Add a list...' />
-				<button onClick={() => handleAddList()}> </button>
-			</div>
+				<div>{reactBoardsArray}</div>
 
-			<div>{reactListArray}</div>
+				</row>
 		</div>
 	);
 };
